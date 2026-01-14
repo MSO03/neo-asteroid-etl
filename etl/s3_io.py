34e701495c032a909027af89e4f4d4faf5c6161a
MSO03 <mishek.sambahangphe.303@my.csun.edu>
2026-01-13 18:36:53 -0800

    Fixed code typos in s3 write code

--- etl/s3_io.py
+++ etl/s3_io.py
@@ -1,14 +1,16 @@
-import boto3
 import json
+import boto3
 
-def write_json_to_s3(bucket : str, key : str, payload:dict ) -> str:
-	s3 = boto3.client("s3")
-	body = json.dumps.(payload, separators=(",",":"), ensure_ascii=False).encode("utf-8")
-	s3.put_object(
-		Bucket=bucket,
-		Key=key,
-		Body=body,
-		ContentType="application/json"
-	)
 
-	return f"s3://{bucket}/{key}"
+def write_json_to_s3(bucket: str, key: str, payload: dict) -> str:
+    s3 = boto3.client("s3")
+    body = json.dumps(payload, separators=(",", ":"), ensure_ascii=False).encode("utf-8")
+
+    s3.put_object(
+        Bucket=bucket,
+        Key=key,
+        Body=body,
+        ContentType="application/json",
+    )
+    return f"s3://{bucket}/{key}"
+
