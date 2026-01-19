e25b63251c778b3df40c507b34ce6304733f25f5
MSO03 <mishek.sambahangphe.303@my.csun.edu>
2026-01-18 19:43:07 -0800

    running as docker container

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,5 @@
-# pipeline.py
 import os
-from prefect import flow, task
+# from prefect import flow, task
 from datetime import date, timedelta
 from dotenv import load_dotenv
 
@@ -10,7 +9,7 @@ from etl.load import load_to_postgres
 from etl.s3_io import write_json_to_s3
 
 
-@task
+# @task
 def extract():
     # last 2 days by default so view has some stats
     end = date.today()
@@ -18,34 +17,36 @@ def extract():
     raw_json, rows = get_neo_data(start_date=start, end_date=end)
     return raw_json, rows
 
-@task
+"""
 def write_raw_to_s3(raw_json):
 	bucket = os.environ["s3_BUCKET"]
 	prefix = os.getenv("s3_PREFIX","raw/neows")
 	d = date.today().isoformat()
 	key = f"{prefix}/date={d}/feed.json"
 	return write_json_to_s3(bucket,key,raw_json)
+"""
 
-@task
+# @task
 def transform(rows):
     return clean_data(rows)
 
 
-@task
+# @task
 def load(rows):
     load_to_postgres(rows)
 
 
-@flow
+# @flow
 def neo_pipeline():
     load_dotenv()
 
     raw_json,rows = extract()
-    s3_uri = write_raw_to_s3(raw_json)
+    # s3_uri = write_raw_to_s3(raw_json)
     cleaned = transform(rows)
     load(cleaned)
 
-    print (f"Wrote raw to:{s3_uri}")
+    # print (f"Wrote raw to:{s3_uri}")
+    print ("Ran pipeline.py")
 
 if __name__ == "__main__":
     neo_pipeline()
